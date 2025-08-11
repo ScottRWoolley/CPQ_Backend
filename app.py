9e120abd007f3ba8d09d8f78dd32cc68efa26b98
Scott <mistermindyt@gmail.com>
2025-08-10 23:32:02 -0700

    Adds CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
+CORS(app)  # Allow all origins by default
 
 # Load MongoDB URI from environment variable
 MONGO_URI = os.environ.get("MONGO_URI")
@@ -11,7 +13,8 @@ client = MongoClient(MONGO_URI)
 @app.route("/answers", methods=["POST"])
 def answers():
     data = request.get_json()
+    return jsonify({"status": "received"}), 200
 
 @app.route("/get_question", methods=["GET"])
 def get_question():
-    return(100)
+    return jsonify(100)
