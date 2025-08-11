42a9beb4716972480132f6d905aa7b32ba3b9ff5
Scott <143149489+ScottRWoolley@users.noreply.github.com>
2025-08-10 23:51:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,22 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 import os
-import json
 
 app = Flask(__name__)
-CORS(app)  # Allow all origins by default
+CORS(app)  # Enable CORS for all routes
 
-# Load MongoDB URI from environment variable
 MONGO_URL = os.environ.get("MONGO_URL")
 client = MongoClient(MONGO_URL)
 db = client.Data
 
-def get_data(collection, query = dict()):
-        documents = list(db[collection].find(query))
-        filtered = []
-        for document in documents:
-            document.pop("_id")
-            filtered.append(document)
-        return filtered
+def get_data(collection, query=dict()):
+    documents = list(db[collection].find(query))
+    filtered = []
+    for document in documents:
+        document.pop("_id", None)
+        filtered.append(document)
+    return filtered
+
 def upload_data(collection, data):
     try:
         if isinstance(data, list) and data:
@@ -26,17 +25,31 @@ def upload_data(collection, data):
         elif data and isinstance(data, dict):
             db[collection].insert_one(data)
         else:
-            print.warning(
-                f'Data for insertion to "{collection}" is not a list or dictionary, or is empty'
-            )
+            print(f'Warning: Data for insertion to "{collection}" is not a list or dict, or empty')
     except Exception as err:
-        print(f"Unable to insert some documents into collection {collection}: {err}")
+        print(f"Unable to insert into {collection}: {err}")
 
 @app.route("/answers", methods=["POST"])
 def answers():
     data = request.get_json()
-    return jsonify({"status": "received"}), 200
+    upload_data("Answers", data)
+    return jsonify({"status": "success"}), 200
 
-@app.route("/get_question", methods=["GET"])
-def get_question():
-    return jsonify(100)
+@app.route("/get_questions", methods=["GET"])
+def get_questions():
+    questions = get_data("Questions")
+    if questions:
+        return jsonify(questions[0])
+    else:
+        return jsonify({}), 404
+
+@app.route("/get_points", methods=["GET"])
+def get_points():
+    points = get_data("Points")
+    if points:
+        return jsonify(points[0])
+    else:
+        return jsonify({}), 404
+
+if __name__ == "__main__":
+    app.run()
